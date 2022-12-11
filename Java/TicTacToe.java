f1fe4535b9ea1ffe252f6d0c0a00f2fa704ef0df
Mark Hegedus <h.marci9988@gmail.com>
2022-12-11 22:25:20 +0100

    When game starts, choice selection was added

--- Java/TicTacToe.java
+++ Java/TicTacToe.java
@@ -2,30 +2,53 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    String name;
 
+    public TicTacToe(){
 
-        
-    // public TicTacToe(String name){
-    //     this.name = name;
-    // }
-    
+    }
 
-    
+    public void Start(){
+        Scanner choice = new Scanner(System.in);
+        System.out.println("1 - Start New Game \n2 - Read rules");
+        switch (choice.nextLine()){
 
+            case "1":
+            System.out.println("This will start a game eventually");
+            break;
+
+            case "2":
+            System.out.println("This is going to be rules eventually");
+            break;
+
+            default:
+            System.out.println("Incorrect statement was entered, please choose a correct statement from the list above.");
+            Start();
+        }
+    }
 
     public static void main(String[] args) {
-        Scanner inputName = new Scanner(System.in);     //Creat scanner 
-        
+        TicTacToe currentGame = new TicTacToe();
+
+        Scanner input= new Scanner(System.in);     //Creat scanner 
         System.out.print("Player One, please enter your name: ");
-        Player playerOne = new Player(inputName.nextLine());     //Create new user based on input
+        Player playerOne = new Player(input.nextLine());     //Create new user based on input
         System.out.print("Player Two, please enter your name: ");
-        Player playerTwo = new Player(inputName.nextLine());     //Create new user based on input
-        inputName.close();      //Closing scanner
-
-        System.out.println("Welcome "+playerOne.name+" and "+playerTwo.name);
- 
+        Player playerTwo = new Player(input.nextLine());     //Create new user based on input
+        // inputName.close();      //Closing scanner
 
 
+        System.out.println("Welcome "+playerOne.name+" and "+playerTwo.name+ " the game shell begin. \nPlease select one of the following by typing it in.");
+
+        currentGame.Start();
+        // System.out.println("1 - Start New Game \n 2 - Read rules");
+        // System.out.println(choice.nextLine());
+        // if (choice.nextLine().equals("1")){
+        //     System.out.println("This will start a game eventually");
+        // } else if (choice.nextLine().equals("2")){
+        //     System.out.println("This is going to be rules eventually");
+        // } else {
+        //     System.out.println("Incorrect statement was added");
+        //     // Start();
+        // }
     }
 }
