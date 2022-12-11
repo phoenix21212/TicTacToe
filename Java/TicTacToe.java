7ea272d826b74b7f681a54b268dee7b7473ae36e
Márk Hegedűs <markhegedus@Marks-MacBook-Air-2.local>
2022-12-11 12:42:01 +0100

    Name input added

--- Java/TicTacToe.java
+++ Java/TicTacToe.java
@@ -1,8 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class TicTacToe {
 
+    String name;
+
+    String[][] table = new String[][]{{" "," "," "},
+                                      {" "," "," "},
+                                      {" "," "," "}
+                                     };
+        
+    public TicTacToe(String name){
+        this.name = name;
+    }
+    
 
     
+
+
     public static void main(String[] args) {
-        
+        Scanner inputName = new Scanner(System.in);  //creat scanner 
+        System.out.println("Enter your name: ");
+        TicTacToe myGame = new TicTacToe(inputName.nextLine()); //create object based on input
+
+        System.out.println("Welcome "+myGame.name+",");
+        // System.out.println(Arrays.toString(table));
     }
 }
